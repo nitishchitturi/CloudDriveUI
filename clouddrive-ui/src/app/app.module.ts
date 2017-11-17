bf06c3b9a7fe4d5c60eeffc07dde0f97507399a1
nitishchitturi <32821645+nitishchitturi@users.noreply.github.com>
2017-11-17 14:23:03 -0800

    App.model.ts changes for merge ocnflict

--- clouddrive-ui/src/app/app.module.ts
+++ clouddrive-ui/src/app/app.module.ts
@@ -1,15 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-<<<<<<< HEAD
 
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
-=======
 import { HttpClientModule } from '@angular/common/http';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { HttpModule } from '@angular/http';
-import { FormsModule } from '@angular/forms'
 import {DataService} from './services/data.service';
->>>>>>> 0d826ab37911f18b5594d6a80499cbe1b7c883b8
 import { AppComponent } from './app.component';
 import { CdfileComponent } from './components/cdfile/cdfile.component';
 import { CdfiledetailsComponent } from './components/cdfiledetails/cdfiledetails.component';
@@ -23,12 +18,7 @@ import { CdfiledetailsComponent } from './components/cdfiledetails/cdfiledetails
   ],
   imports: [
     BrowserModule,
-<<<<<<< HEAD
     FormsModule
-=======
-    HttpModule,
-    HttpClientModule,
->>>>>>> 0d826ab37911f18b5594d6a80499cbe1b7c883b8
   ],
   providers: [ DataService,],
   bootstrap: [AppComponent]

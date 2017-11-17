ea777887554f08006a11a4e845ceab0bb37e51a4
Emmanuel Ogala <ogalaemmanuel@gmail.com>
2017-11-17 12:33:40 -0800

    cdfiles.ts updated

--- clouddrive-ui/src/app/cdfile.ts
+++ clouddrive-ui/src/app/cdfile.ts
@@ -1,21 +1,6 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 
 
 //An interface for the expected file object
-export class CdFile {
-    id: Number;
-    parent_id: Number;
-    name: String;
-    path: String;
-    fileType: Number;
-    size: Number;
-  
-  
-  }
-=======
-=======
->>>>>>> 132399bb5ea83e6aca2a83074c4b780bece650c0
 export class CDFile{
     id: number;
     name: string;
@@ -24,9 +9,4 @@ export class CDFile{
     parentid: number;
     size: number;
     sha: string;
-<<<<<<< HEAD
 }
->>>>>>> 132399bb5ea83e6aca2a83074c4b780bece650c0
-=======
-}
->>>>>>> 132399bb5ea83e6aca2a83074c4b780bece650c0
